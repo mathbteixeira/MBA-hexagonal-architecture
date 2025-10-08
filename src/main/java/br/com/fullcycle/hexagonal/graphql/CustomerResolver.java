3076672ea3a3a3d8b88456365a857383c7078a8b
Matheus Teixeira <matheus_teixeira@anfcorp.com>
2025-10-07 22:31:36 -0300

    implementando usecase no CustomerResolver e Controller + unit tests

--- src/main/java/br/com/fullcycle/hexagonal/graphql/CustomerResolver.java
+++ src/main/java/br/com/fullcycle/hexagonal/graphql/CustomerResolver.java
@@ -1,15 +1,13 @@
 package br.com.fullcycle.hexagonal.graphql;
 
+import br.com.fullcycle.hexagonal.application.usecases.CreateCustomerUseCase;
 import br.com.fullcycle.hexagonal.dtos.CustomerDTO;
-import br.com.fullcycle.hexagonal.models.Customer;
 import br.com.fullcycle.hexagonal.services.CustomerService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
-import java.net.URI;
-
+// Adapter
 @Controller
 public class CustomerResolver {
 
@@ -20,22 +18,9 @@ public class CustomerResolver {
     }
 
     @MutationMapping
-    public CustomerDTO createCustomer(@Argument CustomerDTO dto) {
-        if (customerService.findByCpf(dto.getCpf()).isPresent()) {
-            throw new RuntimeException("Customer already exists");
-        }
-        if (customerService.findByEmail(dto.getEmail()).isPresent()) {
-            throw new RuntimeException("Customer already exists");
-        }
-
-        var customer = new Customer();
-        customer.setName(dto.getName());
-        customer.setCpf(dto.getCpf());
-        customer.setEmail(dto.getEmail());
-
-        customer = customerService.save(customer);
-
-        return new CustomerDTO(customer);
+    public CreateCustomerUseCase.Output createCustomer(@Argument CustomerDTO input) {
+        final var useCase = new CreateCustomerUseCase(customerService);
+        return useCase.execute(new CreateCustomerUseCase.Input(input.getCpf(), input.getEmail(), input.getName()));
     }
 
     public CustomerDTO customerOfId(@Argument Long id) {
