0b8be0fe57507ea16c764e1d3ed66c8b2d0988f8
Matheus Teixeira <matheus_teixeira@anfcorp.com>
2025-10-20 22:32:23 -0300

    criando teste integration de CreatePartnerUseCase

---
+++ src/test/java/br/com/fullcycle/hexagonal/infrastructure/application/usecases/CreatePartnerUseCaseIT.java
@@ -0,0 +1,96 @@
+package br.com.fullcycle.hexagonal.infrastructure.application.usecases;
+
+import br.com.fullcycle.hexagonal.IntegrationTest;
+import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
+import br.com.fullcycle.hexagonal.application.usecases.CreatePartnerUseCase;
+import br.com.fullcycle.hexagonal.infrastructure.models.Partner;
+import br.com.fullcycle.hexagonal.infrastructure.repositories.PartnerRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CreatePartnerUseCaseIT extends IntegrationTest {
+
+    @Autowired
+    private CreatePartnerUseCase useCase;
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @BeforeEach
+    void tearDown() {
+        partnerRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Deve criar um parceiro")
+    public void testCreatePartner() {
+        //given
+        final var expectedCNPJ = "41536538000100";
+        final var expectedEmail = "john.doe@gmail.com";
+        final var expectedName = "John Doe";
+
+        final var createInput = new CreatePartnerUseCase.Input(expectedCNPJ, expectedEmail, expectedName);
+
+        //when
+        final var output = useCase.execute(createInput);
+
+        //then
+        Assertions.assertNotNull(output.id());
+        Assertions.assertEquals(expectedCNPJ, output.cnpj());
+        Assertions.assertEquals(expectedEmail, output.email());
+        Assertions.assertEquals(expectedName, output.name());
+    }
+
+    @Test
+    @DisplayName("Não deve criar um parceiro com CNPJ duplicado")
+    public void testCreateWithDuplicatedCNPJShouldFail() {
+        //given
+        final var expectedCNPJ = "41536538000100";
+        final var expectedEmail = "john.doe@gmail.com";
+        final var expectedName = "John Doe";
+        final var expectedError = "Partner already exists";
+
+        createPartner(expectedCNPJ, expectedEmail, expectedName);
+
+        final var createInput = new CreatePartnerUseCase.Input(expectedCNPJ, expectedEmail, expectedName);
+
+        //when
+        final var actualException = assertThrows(ValidationException.class, () -> useCase.execute(createInput));
+
+        //then
+        Assertions.assertEquals(expectedError, actualException.getMessage());
+    }
+
+    @Test
+    @DisplayName("Não deve criar um parceiro com Email duplicado")
+    public void testCreateWithDuplicatedEmailShouldFail() {
+        //given
+        final var expectedCNPJ = "41536538000100";
+        final var expectedEmail = "john.doe@gmail.com";
+        final var expectedName = "John Doe";
+        final var expectedError = "Partner already exists";
+
+        createPartner("12345678901234", expectedEmail, expectedName);
+
+        final var createInput = new CreatePartnerUseCase.Input(expectedCNPJ, expectedEmail, expectedName);
+
+        //when
+        final var actualException = assertThrows(ValidationException.class, () -> useCase.execute(createInput));
+
+        //then
+        Assertions.assertEquals(expectedError, actualException.getMessage());
+    }
+
+    private Partner createPartner(final String cnpj, final String email, final String name) {
+        final Partner aPartner = new Partner();
+        aPartner.setCnpj(cnpj);
+        aPartner.setEmail(email);
+        aPartner.setName(name);
+        return partnerRepository.save(aPartner);
+    }
+}
